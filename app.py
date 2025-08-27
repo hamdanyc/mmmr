d7af4382383a87c8f2f7c472d2ee8051fc3907d7
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-08-27 13:19:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ else:
 
 # 1. Table Layout Grid (Responsive)
 st.markdown("<h2 style='text-align: right; color: red; font-weight: bold'</h2>"f"⏳ Countdown: {countdown_text}", unsafe_allow_html=True)
-st.markdown("<h1 style='text-align: center; color: blue;'>Majlis Makan Malam RAFOC | 14 Dis 2025</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: blue; font-weight: bold'>Majlis Makan Malam RAFOC | 14 Dis 2025</h2>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center; color: blue; font-weight: bold'>Status Tempahan Meja | Tajaan | Tetamu</h2>", unsafe_allow_html=True)
 st.markdown("<h3 style='color: #00008B;'>🗺️ Tempahan Meja</h3>", unsafe_allow_html=True)
 
