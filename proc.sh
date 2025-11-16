ff81276e71cf4f969ae37559dea2831629f46a4a
hamdan <hamdan.hy@gmail.com>
2025-11-16 19:12:03 +0800

    re-run

--- proc.sh
+++ proc.sh
@@ -1,5 +1,5 @@
 echo "seat allocation -- start"
-uv run guest_seat_assign.py
+uv run guest_seat_gem.py
 sed -i 's/Tudm/TUDM/g;s/Mmu/MMU/g;s/Tldm/TLDM/g;s/Apmm/APMM/g;s/Jhev/JHEV/g;s/Pvatm/PVATM/g;s/Ramd/RAMD/g;s/Pgb/PGB/g;s/Kagat/KAGAT/g;s/Ugat/UGAT/g;s/Kko/KKO/g;s/Ppp/PPP/g;s/Ydp/YDP/g;s/Adc/ADC/g;s/Hs/HS/g' guest_seat.csv
 uv run guest_seat_pdf.py
 uv run guest_summary.py
