8e6d446f79c3385bfb305e91318584cb9f8092c8
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-11-18 15:49:50 +0800

    Refactor sed command for guest_seat.csv
    Updated sed command to include additional substitutions for guest_seat.csv.

--- proc.sh
+++ proc.sh
@@ -1,6 +1,9 @@
 echo "seat allocation -- start"
 uv run guest_seat_gem.py
-sed -i 's/Tudm/TUDM/g;s/Mmu/MMU/g;s/Tldm/TLDM/g;s/Apmm/APMM/g;s/Jhev/JHEV/g;s/Pvatm/PVATM/g;s/Ramd/RAMD/g;s/Pgb/PGB/g;s/Kagat/KAGAT/g;s/Ugat/UGAT/g;s/Kko/KKO/g;s/Ppp/PPP/g;s/Ydp/YDP/g;s/Adc/ADC/g;s/Hs/HS/g' guest_seat.csv
+# sed -E 's/\<[[:alpha:]]{3,}\>/\U&/g'
+sed -i 's/Pat/PAT/g;s/Ptd/PTD/g;s/Ptl/PTL/g;s/Ptu/PTU/g;s/Tudm/TUDM/g;s/Mmu/MMU/g;s/Tldm/TLDM/g;s/Apmm/APMM/g; 
+    s/Jhev/JHEV/g;s/Pvatm/PVATM/g;s/Ramd/RAMD/g;s/Pgb/PGB/g;s/Kagat/KAGAT/g; 
+    s/Ugat/UGAT/g;s/Pernama/PERNAMA/g;s/Dymm/DYMM/g;s/Yb/YB/g;s/Adc/ADC/g;s/Hs/HS/g' guest_seat.csv
 uv run guest_seat_pdf.py
 uv run guest_summary.py
 uv run guest_list.py
