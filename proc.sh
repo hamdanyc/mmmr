559d6509fe13535a6ff4672281a0607cf5c1e62c
hamdan <hamdan.hy@gmail.com>
2025-11-20 15:41:19 +0800

    Modified

--- proc.sh
+++ proc.sh
@@ -1,10 +1,10 @@
 echo "seat allocation -- start"
 uv run guest_seat_gem.py
-# sed -E 's/\<[[:alpha:]]{3,}\>/\U&/g'
-sed -i 's/Pat/PAT/g;s/Ptd/PTD/g;s/Ptl/PTL/g;s/Ptu/PTU/g;s/Tudm/TUDM/g;s/Mmu/MMU/g;s/Tldm/TLDM/g;s/Apmm/APMM/g;s/diraja/Diraja/g;
-    s/Othman-Yaakob/Othman Yaakob Lt Kol (B)/g;s/Nawawi-Desa/Dato Nawawi Desa Lt Kol (B)/g;s/murthy/Dato Dr Murthy Mej (B)/g;
-    s/Jhev/JHEV/g;s/Pvatm/PVATM/g;s/Ramd/RAMD/g;s/Pgb/PGB/g;s/Kagat/KAGAT/g;s/rma-sandhurst/RMA Sandhurst/g;s/dmhk/DMHK/g;
-    s/Ramli-Kinta/Ramli Kinta Lt Kol (B)/g;s/Ugat/UGAT/g;s/Pernama/PERNAMA/g;s/Dymm/DYMM/g;s/Yb/YB/g;s/Adc/ADC/g;s/Hs/HS/g' guest_seat.csv
+# sed -E 's/\<[[:alpha:]]{3,}\>/\U&/gI'
+sed -i 's/Pat/PAT/gI;s/Ptd/PTD/gI;s/Ptl/PTL/gI;s/Ptu/PTU/gI;s/Tudm/TUDM/gI;s/Mmu/MMU/gI;s/Tldm/TLDM/gI;s/Apmm/APMM/gI;s/diraja/Diraja/gI;
+    s/Othman-Yaakob/Othman Yaakob Lt Kol (B)/gI;s/Nawawi-Desa/Dato Nawawi Desa Lt Kol (B)/gI;s/murthy/Dato Dr Murthy Mej (B)/gI;
+    s/Jhev/JHEV/gI;s/Pvatm/PVATM/gI;s/Ramd/RAMD/gI;s/Pgb/PGB/gI;s/Kagat/KAGAT/gI;s/rma-sandhurst/RMA Sandhurst/gI;s/dmhk/DMHK/gI;
+    s/Ramli-Kinta/Ramli Kinta Lt Kol (B)/gI;s/Ugat/UGAT/gI;s/Pernama/PERNAMA/gI;s/Dymm/DYMM/gI;s/Yb/YB/gI;s/Adc/ADC/gI;s/Hs/HS/gI' guest_seat.csv
 uv run guest_seat_pdf.py
 uv run guest_summary.py
 uv run guest_list.py
