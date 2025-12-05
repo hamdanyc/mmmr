3fd75409fde96d9a9f5feadf1c2f33d8cb8fab7c
hamdan <hamdan.hy@gmail.com>
2025-12-06 04:27:22 +0800

    Modify target guest

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ with col1:
 with col2:
     total_simpanan = tetamu_df['name'].str.contains('simpanan', case=False).sum()
     total_guests = len(tetamu_df) - total_simpanan
-    guests_target = 560
+    guests_target = 504
     guests_percentage = (total_guests / guests_target) * 100 if guests_target > 0 else 0
 
     fig_guests = go.Figure(go.Indicator(
