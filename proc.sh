98c90d6f5c35777826df731ac6dc479274fd2827
hamdan <hamdan.hy@gmail.com>
2025-12-06 20:02:04 +0800

    Minor update

--- proc.sh
+++ proc.sh
@@ -4,7 +4,7 @@ uv run guest_seat_gem.py
 sed -i 's/Pat/PAT/gI;s/Ptd/PTD/gI;s/Ptl/PTL/gI;s/Ptu/PTU/gI;s/Tudm/TUDM/gI;s/Mmu/MMU/gI;s/Tldm/TLDM/gI;s/Apmm/APMM/gI;s/diraja/Diraja/gI;
     s/Othman-Yaakob/Othman Yaakob Lt Kol (B)/gI;s/Nawawi-Desa/Dato Nawawi Desa Lt Kol (B)/gI;s/murthy/Dato Dr Murthy Mej (B)/gI;
     s/Jhev/JHEV/gI;s/Pvatm/PVATM/gI;s/Ramd/RAMD/gI;s/Pgb/PGB/gI;s/Kagat/KAGAT/gI;s/rma-sandhurst/RMA Sandhurst/gI;s/dmhk/DMHK/gI;
-    s/Ramli-Kinta/Ramli Kinta Lt Kol (B)/gI;s/Ugat/UGAT/gI;s/Pernama/PERNAMA/gI;s/Dymm/DYMM/gI;s/Yb/YB/gI;s/Adc/ADC/gI;s/Hs/HS/gI' guest_seat.csv
+    s/Ramli-Kinta/Ramli Kinta Lt Kol (B)/gI;s/ssc26l/SSC 26 TLDM/gI;s/Pernama/PERNAMA/gI;s/Dymm/DYMM/gI;s/Yb/YB/gI;s/Adc/ADC/gI;s/Hs/HS/gI' guest_seat.csv
 uv run guest_seat_pdf.py
 uv run guest_summary.py
 uv run guest_list.py
